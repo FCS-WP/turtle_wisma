361ce58fb75809fb931b7dd00e8dbee361267e23
toanpham2111 <toan444666@gmail.com>
2025-04-03 18:11:41 +0700

    update

--- src/wp-content/themes/zippy-elementor-child/assets/js/app.js
+++ src/wp-content/themes/zippy-elementor-child/assets/js/app.js
@@ -10,10 +10,41 @@ $(document).ready(function () {
     infinite: false,
     dots: false,
     arrows: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+        },
+      },
+    ]
   });
   $("[data-fancybox='gallery-slicks']").fancybox({
     toolbar: true,
     thumbs: false,
     hash: false,
   });
+
+  // $(".fancybox-trigger img").on("click", function () {
+  //   if ($(this).closest(".fancybox-trigger").length) {
+  //     let gallery = [];
+  //     $(".fancybox-trigger img").each(function () {
+  //       gallery.push({ src: $(this).attr("src"), type: "image" });
+  //     });
+
+  //     let index = $(".fancybox-trigger img").index(this);
+
+  //     $.fancybox.open(gallery, { index: index });
+  //   }
+  // });
 });
